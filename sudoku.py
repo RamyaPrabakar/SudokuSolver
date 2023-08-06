2228640daf87caa5191ff075c9266addca568529
Ramya Prabakar <ramyap02@uw.edu>
2023-08-06 16:33:42 -0700

    Added functions to find empty space on board and to check if adding a number to a position is valid

--- sudoku.py
+++ sudoku.py
@@ -23,3 +23,31 @@ def print_board(board):
                 print(board[i][j])
             else:
                 print(str(board[i][j]) + " ", end = " ")
+
+# Finds an empty space on the board
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i,j) # row, column
+    return None
+
+def valid(board, num, pos):
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+        
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+    
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3 , box_y * 3 + 3):
+        for j in range(box_x * 3 , box_x * 3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
